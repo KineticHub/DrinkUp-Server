f5a9d702632cf918a3ab3e844f1e6b5e7081209a
Kinetic Alnajar <kalnajar09@gmail.com>
2013-02-22 15:54:09 -0500

    working on orders admin

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -39,10 +39,10 @@ class DrinkModelAdmin(FilterUserAdmin):
 class DrinkTypeModelAdmin(FilterUserAdmin):
 	exclude = ('user',)
 	
-class OrderModelAdmin(FilterUserAdmin):
+class OrderModelAdmin(admin.ModelAdmin):
 
 	def queryset(self, request): 
-		qs = super(FilterUserAdmin, self).queryset(request)
+		qs = super(OrderModelAdmin, self).queryset(request)
 		bars = Bar.objects.filter(user=request.user)
 		return qs.filter(bar__pk__in = [bar.pk for bar in bars])
 		#return qs.filter(user=request.user)
