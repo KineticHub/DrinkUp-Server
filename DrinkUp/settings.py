b2f060c8bfbd1de8bf9e1b84a6c32fbde802b27a
K <kalnajar09@gmail.com>
2013-02-22 01:49:16 -0500

    updated settings to include apps

--- DrinkUp/settings.py
+++ DrinkUp/settings.py
@@ -115,6 +115,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ApiApp',
+    'MainApp',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
