cf2a3ba1b3c0979f6e9f605e3bed8cca6c2ae59e
K <kalnajar09@gmail.com>
2013-02-22 02:03:53 -0500

    add django_extensions to settings

--- DrinkUp/settings.py
+++ DrinkUp/settings.py
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'ApiApp',
     'MainApp',
     # Uncomment the next line to enable the admin:
