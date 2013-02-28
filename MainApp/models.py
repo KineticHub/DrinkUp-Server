d195ef2aff0f063ec1582a644d190a849acf31d1
Kinetic Alnajar <kalnajar09@gmail.com>
2013-02-27 20:45:58 -0500

    added facebookuser and oauthtoken models

--- MainApp/models.py
+++ MainApp/models.py
@@ -2,9 +2,11 @@
 from django.db import models
 from ApiApp.models import BaseModel
 
+from facepy import GraphAPI
+
 class Bar(BaseModel):
-	email = models.EmailField(max_length=254)
-	name = models.CharField(max_length=200)
+	email = models.EmailField(max_length=255)
+	name = models.CharField(max_length=255)
 	icon = models.URLField()
 	lattitude = models.FloatField()
 	longitude = models.FloatField()
@@ -15,25 +17,82 @@ class Bar(BaseModel):
 		
 	class Meta:
 		ordering = ['name']
+		
 
 class AppUser(BaseModel):
 	
 	Gender_Options = (('Male', 'Male'), ('Female','Female'), ('Transgender','Transgender'))
 
-	nickname = models.CharField(max_length=200)
-	email = models.EmailField(max_length=254)
-	#secret = models.CharField(max_length=200)
-	#salt = models.CharField(max_length=200)
-	age = models.PositiveIntegerField()
-	sex = models.CharField(choices=Gender_Options, max_length=15)
-	zipcode = models.PositiveIntegerField()
-	favorite = models.CharField(max_length=200)
-
+	first_name = models.CharField(max_length=255, blank=True, null=True)
+	last_name = models.CharField(max_length=255, blank=True, null=True)
+	nickname = models.CharField(max_length=255, blank=True, null=True)
+	email = models.EmailField(max_length=255)
+	birthdate = models.DateField(blank=True)
+	gender = models.CharField(choices=Gender_Options, max_length=15, blank=True)
+	
+	facebook_user = models.OneToOneField('FacebookAppUser', verbose_name=_('Facebook Profile'))
+	
 	def __unicode__(self):
 		return self.nickname
 		
 	class Meta:
 		ordering = ['nickname']
+
+
+class FacebookAppUser(BaseModel):
+	fb_uid = models.BigIntegerField(_('facebook id'), unique=True)
+	fb_email = models.EmailField(max_length=255, blank=True, null=True)
+	oauth_token = models.OneToOneField('OAuthToken', verbose_name=_('OAuth token'))
+
+class OAuthToken(models.Model):
+	"""
+	Instances of the OAuthToken class are credentials used to query
+	the Facebook API on behalf of a user.
+	"""
+
+	token = models.TextField(_('token'))
+	"""A string describing the OAuth token itself."""
+
+	issued_at = models.DateTimeField(_('issued at'))
+	"""A ``datetime`` object describing when the token was issued."""
+
+	expires_at = models.DateTimeField(_('expires at'), null=True, blank=True)
+	"""A ``datetime`` object describing when the token expires (or ``None`` if it doesn't)"""
+
+	@property
+	def expired(self):
+		"""Determine whether the OAuth token has expired."""
+		return self.expires_at < now() if self.expires_at else False
+
+	@property
+	def extended(self):
+		"""Determine whether the OAuth token has been extended."""
+		if self.expires_at:
+			return self.expires_at - self.issued_at > timedelta(days=30)
+		else:
+			return False
+
+	def extend(self):
+		"""Extend the OAuth token."""
+		graph = GraphAPI()
+
+		response = graph.get('oauth/access_token',
+			client_id = FACEBOOK_APPLICATION_ID,
+			client_secret = FACEBOOK_APPLICATION_SECRET_KEY,
+			grant_type = 'fb_exchange_token',
+			fb_exchange_token = self.token
+		)
+
+		components = parse_qs(response)
+
+		self.token = components['access_token'][0]
+		self.expires_at = now() + timedelta(seconds = int(components['expires'][0]))
+
+		self.save()
+
+	class Meta:
+		verbose_name = _('OAuth token')
+		verbose_name_plural = _('OAuth tokens')
 
 class DrinkType(BaseModel):
 	bar = models.ForeignKey(Bar)
