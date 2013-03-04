331ae0b29be78df8531f3a053d17a279bc8faa53
Kinetic Alnajar <kalnajar09@gmail.com>
2013-03-03 21:06:03 -0500

    working on basic views

--- ApiApp/views.py
+++ ApiApp/views.py
@@ -22,59 +22,51 @@ from MainApp.models import *
 from pyfb import Pyfb
 from DrinkUp.settings import FACEBOOK_APP_ID, FACEBOOK_SECRET_KEY, FACEBOOK_REDIRECT_URL
 
-def AllBars(request):
+def AllVenues(request):
+	if request.method == 'GET':
+		venues_to_return = Venue.objects.all()
+		
+		json_serializer = serializers.get_serializer("json")()
+		response = json_serializer.serialize(venues_to_return, ensure_ascii=False)
+		return HttpResponse(response, mimetype="application/json")
 
-    if request.method == 'GET':
-        
-        bars_to_return = Bar.objects.all()
-        
-        json_serializer = serializers.get_serializer("json")()
-	response = json_serializer.serialize(bars_to_return, ensure_ascii=False)
-	return HttpResponse(response, mimetype="application/json")
-
-def AllDrinks(request):
-
-    if request.method == 'GET':
-        
-        drinks_to_return = Drink.objects.filter()
-        
-        json_serializer = serializers.get_serializer("json")()
-	response = json_serializer.serialize(drinks_to_return, ensure_ascii=False)
-	return HttpResponse(response, mimetype="application/json")
+def VenueBars(request, venue_id):
+	if request.method == 'GET':
+		bars_to_return = VenueBar.objects.filter(venue=venue_id)
+		json_serializer = serializers.get_serializer("json")()
+		response = json_serializer.serialize(bars_to_return, ensure_ascii=False)
+		return HttpResponse(response, mimetype="application/json")
 
 def BarDrinks(request, bar_id):
+	if request.method == 'GET':
+		drinks_to_return = Drink.objects.filter(bar=bar_id)
+		json_serializer = serializers.get_serializer("json")()
+		response = json_serializer.serialize(drinks_to_return, ensure_ascii=False)
+		return HttpResponse(response, mimetype="application/json")
 
-    if request.method == 'GET':
-        
-        drinks_to_return = Drink.objects.filter(bar=bar_id)
-        
-        json_serializer = serializers.get_serializer("json")()
-	response = json_serializer.serialize(drinks_to_return, ensure_ascii=False)
-	return HttpResponse(response, mimetype="application/json")
-        
-		
+
 #BEGIN FB VIEWS
 def index(request):
-    return HttpResponse("""<button onclick="location.href='/Project/facebook_login'">Facebook Login</button>""")
+	return HttpResponse("""<button onclick="location.href='/Project/facebook_login'">Facebook Login</button>""")
 
 #This view redirects the user to facebook in order to get the code that allows
 #pyfb to obtain the access_token in the facebook_login_success view
 def facebook_login(request):
 
-    facebook = Pyfb(FACEBOOK_APP_ID)
-    return HttpResponseRedirect(facebook.get_auth_code_url(redirect_uri=FACEBOOK_REDIRECT_URL))
+	facebook = Pyfb(FACEBOOK_APP_ID)
+	return HttpResponseRedirect(facebook.get_auth_code_url(redirect_uri=FACEBOOK_REDIRECT_URL))
 
 #This view must be refered in your FACEBOOK_REDIRECT_URL. For example: http://www.mywebsite.com/facebook_login_success/
 def facebook_login_success(request):
 
-    code = request.GET.get('code')
+	code = request.GET.get('code')
 
-    facebook = Pyfb(FACEBOOK_APP_ID)
-    facebook.get_access_token(FACEBOOK_SECRET_KEY, code, redirect_uri=FACEBOOK_REDIRECT_URL)
-    me = facebook.get_myself()
+	facebook = Pyfb(FACEBOOK_APP_ID)
+	facebook.get_access_token(FACEBOOK_SECRET_KEY, code, redirect_uri=FACEBOOK_REDIRECT_URL)
+	me = facebook.get_myself()
 
-    welcome = "Welcome <b>%s</b>. Your Facebook login has been completed successfully!"
-    return HttpResponse(welcome % me.__dict__)
+	welcome = "Welcome <b>%s</b>. Your Facebook login has been completed successfully!"
+	return HttpResponse(welcome % me.__dict__)
 
 @csrf_exempt
 def facebook_mobile_login(request):
@@ -92,10 +84,10 @@ def facebook_mobile_login(request):
 		if token and expiration and creation:
 			new_token = OAuthToken(token = token, issued_at = datetime.datetime.fromtimestamp(float(creation)), expires_at = datetime.datetime.fromtimestamp(float(expiration)))
 			new_token.save()
-                        facebook = Pyfb(FACEBOOK_APP_ID)
-                        facebook.set_access_token(token)
-                        me = facebook.get_myself()
-                        return HttpResponse(me.__dict__)
+						facebook = Pyfb(FACEBOOK_APP_ID)
+						facebook.set_access_token(token)
+						me = facebook.get_myself()
+						return HttpResponse(me.__dict__)
 		
 			#new_user = FacebookAppUser(user_id = primary_user, fb_uid = facebook_id, fb_email = facebook_email, oauth_token = new_token)
 			#new_user.save()
