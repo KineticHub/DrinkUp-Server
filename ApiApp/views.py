032330d69661a0fbb1a5d37cc4ad27162e38dade
Kinetic Alnajar <kalnajar09@gmail.com>
2013-03-24 16:45:00 -0400

    order creation debugging 4

--- ApiApp/views.py
+++ ApiApp/views.py
@@ -147,8 +147,9 @@ def CreateNewOrder(request):
 			if bar_id and total and tax and sub_total and tip and fees and grand_total:
 							#response = json.dumps({'status': 'inside',})
 							#return HttpResponse(response, mimetype="application/json")
-					
-							new_order = Order(bar=bar_id, appuser=appuser, total=total, tax=tax, sub_total=sub_total, tip=tip, fees=fees, grand_total=grand_total, current_status=1, description=description)
+							bar = VenueBar.objects.get(pk=bar_id)
+							
+							new_order = Order(bar=bar, appuser=appuser, total=total, tax=tax, sub_total=sub_total, tip=tip, fees=fees, grand_total=grand_total, current_status=1, description=description)
 							new_order.save()
 
 							serialized_response = serializers.serialize('json', [ new_order, ])
