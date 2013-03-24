ba54028349cb98b473460e64dc06bc7a39a330f9
K <kalnajar09@gmail.com>
2013-03-24 09:41:09 -0400

    remove appuser from check in create order

--- ApiApp/views.py
+++ ApiApp/views.py
@@ -141,7 +141,7 @@ def CreateNewOrder(request):
 		grand_total = request.POST.get('grand_total', None)
 		description = request.POST.get('description', '')
 
-		if bar_id and appuser_id and total and tax and sub_total and tip and fees and grand_total:
+		if bar_id and total and tax and sub_total and tip and fees and grand_total:
                         new_order = Order(bar=bar_id, appuser=appuser, total=total, tax=tax, sub_total=sub_total, tip=tip, fees=fees, grand_total=grand_total, current_status=1, description=description)
                         new_order.save()
 
