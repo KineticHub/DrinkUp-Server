30e0aab39f1b05d7ecdb6ffaae388261ecb2d340
K <kalnajar09@gmail.com>
2013-05-02 16:01:54 -0400

    forms import for admin

--- VenueApp/admin.py
+++ VenueApp/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from VenueApp.models import *
 from BarApp.models import *
 
+from django.contrib import admin
+from django import forms
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth.models import Group
