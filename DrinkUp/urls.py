ff92b64483fb8c04c79af16c371d5e165adca075
K <kalnajar09@gmail.com>
2013-05-18 19:58:36 -0400

    fixing URLs

--- DrinkUp/urls.py
+++ DrinkUp/urls.py
@@ -35,9 +35,9 @@ urlpatterns += patterns('ApiApp.views',
 
     url(r'^api/orders/create/$', 'CreateNewOrder'),
     url(r'^api/orders/update/$', 'UpdateOrderStatus'),
-    url(r'^api/orders/(?P<bar_id>\d{1,10})/(?P<status>\d{1,2})/$', 'GetOrdersForBarWithStatus'),
-    url(r'^api/orders/since/(?P<bar_id>\d{1,10})/(?P<since_time>\d{1,10})/(?P<status>\d{1,10})/$', 'GetNewOrdersForBarSince'),
-    url(r'^api/orders/(?P<bar_id>\d{1,10})/(?P<status>\d{1,2})/(?P<time_start>\d{1,10})/(?P<time_end>\d{1,10})/$', 'GetOrdersForBarWithStatusInTimeRange'),
+    url(r'^api/orders/(?P<status>\d{1,2})/$', 'GetOrdersForBarWithStatus'),
+    url(r'^api/orders/since/(?P<since_time>\d{1,10})/(?P<status>\d{1,10})/$', 'GetNewOrdersForBarSince'),
+    url(r'^api/orders/(?P<status>\d{1,2})/(?P<time_start>\d{1,10})/(?P<time_end>\d{1,10})/$', 'GetOrdersForBarWithStatusInTimeRange'),
 
 	url(r'^index/$', 'index'),
 	url(r'^facebook/login/$', 'FacebookLogin'),
