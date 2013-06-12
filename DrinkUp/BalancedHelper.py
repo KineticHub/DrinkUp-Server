c497c695fe400506b175506499d2a2e6a4dc87b3
K <kalnajar09@gmail.com>
2013-06-12 00:13:10 -0400

    another attempt at fixing holds

--- DrinkUp/BalancedHelper.py
+++ DrinkUp/BalancedHelper.py
@@ -146,7 +146,7 @@ class BalancedPaymentsHelper:
 			hold = balanced.Hold(
 				amount=int(round(float(order.grand_total), 2)*100), #this needs to be in pennies
 				description= buyer.name + ' at ' + order.bar.venue.name,
-				source_uri=buyer.cards[0],
+				source_uri=source_uri,
 			).save()
 			return hold
 		except balanced.exc.HTTPError as error:
