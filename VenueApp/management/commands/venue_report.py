c62e646b138fa55edc50c10b4f46432085cac51f
Kinetic Alnajar <kalnajar09@gmail.com>
2013-07-03 00:37:08 -0400

    moved imports around

--- VenueApp/management/commands/venue_report.py
+++ VenueApp/management/commands/venue_report.py
@@ -1,34 +1,31 @@
-import warnings
+from django.core.management.base import BaseCommand, CommandError
 
+#model imports
+from VenueApp.models import *
+from BarApp.models import *
+from UsersApp.models import *
+
+#BalancedPayments
+import balanced
+from django.conf import settings
+#from DrinkUp.BalancedHelper import BalancedPaymentsHelper
+
+#python imports
+from datetime import datetime, timedelta
+from time import gmtime, strftime
+import warnings
+import csv
+
+import warnings
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
 
 
 class Command(BaseCommand):
-
-    from django.core.management.base import BaseCommand, CommandError
-
-    #python imports
-    from datetime import datetime, timedelta
-    from time import gmtime, strftime
-    import warnings
-    import csv
-
-    #model imports
-    from VenueApp.models import *
-    from BarApp.models import *
-    from UsersApp.models import *
-
-    #BalancedPayments
-    import balanced
-    from django.conf import settings
-    #from DrinkUp.BalancedHelper import BalancedPaymentsHelper
-
     help = 'Sends a report to the venues of orders processed over the past day'
 
     def handle(self, *args, **options):
         self.stdout.write('Reports for day: %r\n\n' % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-
         venues = Venue.objects.all()
         for venue in venues:
             orders = BarOrder.objects.filter(venue=venue, created__gte=datetime.now() - timedelta(hours=24))
