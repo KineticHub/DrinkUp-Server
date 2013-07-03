f922be19309037a756af950563e1a6d753338142
Kinetic Alnajar <kalnajar09@gmail.com>
2013-07-03 00:19:38 -0400

    fixing indents

--- VenueApp/management/commands/venue_report.py
+++ VenueApp/management/commands/venue_report.py
@@ -17,24 +17,24 @@ from django.conf import settings
 #from DrinkUp.BalancedHelper import BalancedPaymentsHelper
 
 with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
+	warnings.simplefilter("ignore")
 
 class Command(BaseCommand):
-    
+
 	help = 'Sends a report to the venues of orders processed over the past day'
 
-    def handle(self, *args, **options):
-        self.stdout.write('Reports for day: %r\n\n' %  strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+	def handle(self, *args, **options):
+		self.stdout.write('Reports for day: %r\n\n' %  strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
-        venues = Venue.objects.all()
-        for venue in venues:
-            orders = BarOrder.objects.filter(venue=venue, created__gte = datetime.now() - timedelta(hours=24))
-            report_title = '{venue_name}_{timestamp}.csv'.format(venue_name=venue.name, timestamp=datetime.now())
-            report = csv.writer(open(report_title, "wb"))
-            report.writerow(["Timestamp","Order ID"])
-            for order in orders:
-                report.writerow([order.created, order.pk])
+		venues = Venue.objects.all()
+		for venue in venues:
+			orders = BarOrder.objects.filter(venue=venue, created__gte = datetime.now() - timedelta(hours=24))
+			report_title = '{venue_name}_{timestamp}.csv'.format(venue_name=venue.name, timestamp=datetime.now())
+			report = csv.writer(open(report_title, "wb"))
+			report.writerow(["Timestamp","Order ID"])
+			for order in orders:
+				report.writerow([order.created, order.pk])
 
 
 
-        self.stdout.write('\n-----------------------------------\n\n')
+		self.stdout.write('\n-----------------------------------\n\n')
