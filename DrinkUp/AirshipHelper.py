c7ffaf108fb160d74596a736e0bea4d6e292a0df
Kinetic Alnajar <kalnajar09@gmail.com>
2013-07-27 14:11:10 -0400

    trying to fix UA

--- DrinkUp/AirshipHelper.py
+++ DrinkUp/AirshipHelper.py
@@ -1,10 +1,16 @@
 import urbanairship
 from django.conf import settings
 
-class AirshipHelper:
-    
-    def __init__(self):
-        self.airship = urbanairship.Airship(settings.UA_APP_KEY_PROD, settings.UA_APP_MASTER_SECRET_PROD)
 
-    def pushMessageForUser(self, message, user, status):
-        self.airship.push({'aps': {'alert': message, 'badge':1, 'sound': 'default'}, 'status':status}, aliases=[user.username])
+class AirshipHelper:
+	def __init__ (self):
+		self.airship = urbanairship.Airship(settings.UA_APP_KEY_PROD, settings.UA_APP_MASTER_SECRET_PROD)
+
+	def pushMessageForUser (self, message, user, status):
+	#self.airship.push({'aps': {'alert': message, 'badge':1, 'sound': 'default'}, 'status':status}, aliases=[user.username])
+
+		push = self.airship.create_push()
+		push.audience = self.airship.or_(self.airship.alias(user.username))
+		push.notification = self.airship.notification(alert=message)
+		push.device_types = self.airship.device_types('ios', 'android')
+		push.send()
