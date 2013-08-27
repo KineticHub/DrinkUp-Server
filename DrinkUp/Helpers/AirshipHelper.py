eab5f2b18a487450470eb11232e4aaa7c7673984
Kinetic Alnajar <kalnajar09@gmail.com>
2013-08-27 15:12:29 -0400

    cleaning up some formatting

--- DrinkUp/Helpers/AirshipHelper.py
+++ DrinkUp/Helpers/AirshipHelper.py
@@ -3,16 +3,16 @@ from django.conf import settings
 
 
 class AirshipHelper:
-	def __init__(self):
-		self.airship = ua.Airship(settings.UA_APP_KEY, settings.UA_APP_MASTER_SECRET)
+    def __init__(self):
+        self.airship = ua.Airship(settings.UA_APP_KEY, settings.UA_APP_MASTER_SECRET)
 
-	def push_message_for_user(self, message, user, status):
-		#self.airship.push({'aps': {'alert': message, 'badge':1, 'sound': 'default'},
-		# 'status':status}, aliases=[user.username])
+    def push_message_for_user(self, message, user, status):
+        #self.airship.push({'aps': {'alert': message, 'badge':1, 'sound': 'default'},
+        # 'status':status}, aliases=[user.username])
 
-		push = self.airship.create_push()
-		push.audience = ua.or_(ua.alias(user.username))
-		push.notification = ua.notification(ios=ua.ios(alert=message, sound='default'),
-		                                    android=ua.android(alert=message))
-		push.device_types = ua.device_types('ios', 'android')
-		push.send()
+        push = self.airship.create_push()
+        push.audience = ua.or_(ua.alias(user.username))
+        push.notification = ua.notification(ios=ua.ios(alert=message, sound='default'),
+                                            android=ua.android(alert=message))
+        push.device_types = ua.device_types('ios', 'android')
+        push.send()
--- DrinkUp/Helpers/ViewsHelper.py
+++ DrinkUp/Helpers/ViewsHelper.py
@@ -5,14 +5,16 @@ import pytz
 
 __author__ = 'Kinetic'
 
+
 def convert_current_UTC_to_venue_local_datetime(venue):
-	utc = datetime.utcnow()
-	utc = utc.replace(tzinfo=pytz.utc)
-	venue_timezone_time = utc.astimezone(venue.timezone)
-	return venue_timezone_time
+    utc = datetime.utcnow()
+    utc = utc.replace(tzinfo=pytz.utc)
+    venue_timezone_time = utc.astimezone(venue.timezone)
+    return venue_timezone_time
+
 
 def convert_current_UTC_to_venue_local_time(venue):
-	return convert_current_UTC_to_venue_local_datetime(venue).time()
+    return convert_current_UTC_to_venue_local_datetime(venue).time()
 
 # def determine_open_venues(venues):
 # 	open_nearby_venues = []
@@ -29,29 +31,29 @@ def convert_current_UTC_to_venue_local_time(venue):
 # 	return open_nearby_venues
 
 def determine_open_venues(venues):
-	open_nearby_venues = []
-	for venue in venues:
-		weekday = convert_current_UTC_to_venue_local_datetime(venue).weekday()
-		yesterday = convert_current_UTC_to_venue_local_datetime(venue).weekday() - 1
-		try:
-			venue_weekday = VenueOpeningHours.objects.get(venue=venue, weekday=weekday)
-		except VenueOpeningHours.DoesNotExist:
-			continue
+    open_nearby_venues = []
+    for venue in venues:
+        weekday = convert_current_UTC_to_venue_local_datetime(venue).weekday()
+        yesterday = convert_current_UTC_to_venue_local_datetime(venue).weekday() - 1
+        try:
+            venue_weekday = VenueOpeningHours.objects.get(venue=venue, weekday=weekday)
+        except VenueOpeningHours.DoesNotExist:
+            continue
 
-		now = convert_current_UTC_to_venue_local_time(venue)
-		if venue_weekday.open_hour < now < venue_weekday.close_hour \
-			and not venue_weekday.closed:
-			open_nearby_venues.append(venue)
+        now = convert_current_UTC_to_venue_local_time(venue)
+        if venue_weekday.open_hour < now < venue_weekday.close_hour \
+            and not venue_weekday.closed:
+            open_nearby_venues.append(venue)
 
-		elif now < venue_weekday.open_hour:
-			try:
-				venue_weekday_yesterday = VenueOpeningHours.objects.get(venue=venue, weekday=yesterday)
-			except VenueOpeningHours.DoesNotExist:
-				continue
+        elif now < venue_weekday.open_hour:
+            try:
+                venue_weekday_yesterday = VenueOpeningHours.objects.get(venue=venue, weekday=yesterday)
+            except VenueOpeningHours.DoesNotExist:
+                continue
 
-			if (venue_weekday_yesterday.open_hour > venue_weekday_yesterday.close_hour) \
-				and (now < venue_weekday_yesterday.close_hour or now > venue_weekday_yesterday.open_hour)\
-				and not venue_weekday_yesterday.closed:
-				open_nearby_venues.append(venue)
+            if (venue_weekday_yesterday.open_hour > venue_weekday_yesterday.close_hour) \
+                and (now < venue_weekday_yesterday.close_hour or now > venue_weekday_yesterday.open_hour) \
+                and not venue_weekday_yesterday.closed:
+                open_nearby_venues.append(venue)
 
-	return open_nearby_venues
+    return open_nearby_venues
